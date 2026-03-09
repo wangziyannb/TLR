4e19aacd63f245842c1a33183552443786765d60
EvanZhangcn <mrzhanglwchn@gmail.com>
2026-03-09 16:12:16 +0000

    qwen, llama2

--- reproduce_wanda_table1_qwen3_8b.sh
+++ reproduce_wanda_table1_qwen3_8b.sh
@@ -9,34 +9,62 @@ WIKITEXT_SEQS=${WIKITEXT_SEQS:-128}
 C4_SEQS=${C4_SEQS:-128}
 RANK=${RANK:-128}
 ITERS=${ITERS:-50}
-LOAD_CALIBRATION_PATH=${LOAD_CALIBRATION_PATH:-/home/zwang53/tlr_repro_codebase/calib_qwen3-8b.pth}
+LOAD_CALIBRATION_PATH=${LOAD_CALIBRATION_PATH:-/workspace/TLR/calib_qwen3-8b.pth}
 
 mkdir -p "$OUT"
 
-for SP in 0.5 0.6 0.7; do
-  for REF in none zerosvd ours; do
+#for SP in 0.5 0.6 0.7; do
+#  for REF in none zerosvd ours; do
+#    python apply_prune_refine.py \
+#      --model "$MODEL" \
+#      --pruning wanda --sparsity "$SP" \
+#      --c4_seqs "$C4_SEQS" --seq_len "$SEQ_LEN" --use_c4_streaming \
+#      --refine "$REF" --rank "$RANK" --iters "$ITERS" \
+#      --eval_ppl --wikitext_seqs "$WIKITEXT_SEQS" \
+#      --load_calibration_path "$LOAD_CALIBRATION_PATH" --export_param_dict \
+#      --output_dir "$OUT/wanda_sp${SP}_${REF}_${RANK}"
+#  done
+#done
+
+#for NM in "4 8" "2 4"; do
+#  N=$(echo $NM | awk '{print $1}')
+#  M=$(echo $NM | awk '{print $2}')
+#  for REF in none zerosvd ours; do
+#    python apply_prune_refine.py \
+#      --model "$MODEL" \
+#      --pruning wanda --nm $N $M \
+#      --c4_seqs "$C4_SEQS" --seq_len "$SEQ_LEN" --use_c4_streaming \
+#      --refine "$REF" --rank "$RANK" --iters "$ITERS" \
+#      --eval_ppl --wikitext_seqs "$WIKITEXT_SEQS" \
+#      --load_calibration_path "$LOAD_CALIBRATION_PATH" --export_param_dict \
+#      --output_dir "$OUT/wanda_${N}of${M}_${REF}_${RANK}"
+#  done
+#done
+
+for SP in 0.5; do
+  for REF in ours; do
     python apply_prune_refine.py \
       --model "$MODEL" \
       --pruning wanda --sparsity "$SP" \
       --c4_seqs "$C4_SEQS" --seq_len "$SEQ_LEN" --use_c4_streaming \
       --refine "$REF" --rank "$RANK" --iters "$ITERS" \
       --eval_ppl --wikitext_seqs "$WIKITEXT_SEQS" \
-      --load_calibration_path "$LOAD_CALIBRATION_PATH" \
+      --load_calibration_path "$LOAD_CALIBRATION_PATH" --export_param_dict \
       --output_dir "$OUT/wanda_sp${SP}_${REF}_${RANK}"
   done
 done
 
-for NM in "4 8" "2 4"; do
+for NM in "2 4"; do
   N=$(echo $NM | awk '{print $1}')
   M=$(echo $NM | awk '{print $2}')
-  for REF in none zerosvd ours; do
+  for REF in ours; do
     python apply_prune_refine.py \
       --model "$MODEL" \
       --pruning wanda --nm $N $M \
       --c4_seqs "$C4_SEQS" --seq_len "$SEQ_LEN" --use_c4_streaming \
       --refine "$REF" --rank "$RANK" --iters "$ITERS" \
       --eval_ppl --wikitext_seqs "$WIKITEXT_SEQS" \
-      --load_calibration_path "$LOAD_CALIBRATION_PATH" \
+      --load_calibration_path "$LOAD_CALIBRATION_PATH" --export_param_dict \
       --output_dir "$OUT/wanda_${N}of${M}_${REF}_${RANK}"
   done
 done
